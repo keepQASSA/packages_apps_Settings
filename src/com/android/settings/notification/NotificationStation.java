73a781c5138d0d858f3b22fabee5e09d70811a3d
Alex <du.alexcruz@gmail.com>
2020-03-17 15:42:27 -0400

    Expose the notification log
    Google added a way to see your past notifications but is buried. The stock
    hoes were accessing this by way of the Settings shortcuts but I feel like
    is time for our users to see this without having to do any fuckery.
    
    In case you don't know what WTF this is...
    
    androidpit.com/how-to-recover-lost-notifications-on-android
    
    Change-Id: I735b3d6c0a94f3d5bed867a2988056f6545df1df

--- src/com/android/settings/notification/NotificationStation.java
+++ src/com/android/settings/notification/NotificationStation.java
@@ -205,6 +205,9 @@ public class NotificationStation extends SettingsPreferenceFragment implements I
     public void onResume() {
         logd("onResume()");
         super.onResume();
+        // Set correct title on the action bar
+        getActivity().setTitle(R.string.notification_log_title);
+
         try {
             mListener.registerAsSystemService(mContext, new ComponentName(mContext.getPackageName(),
                     this.getClass().getCanonicalName()), ActivityManager.getCurrentUser());
