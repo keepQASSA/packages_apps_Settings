aa42d2ea3928542413734ae96c74835dadb5c563
xyzuan <xyzuannihboss@gmail.com>
2021-07-17 04:17:49 +0000

    xdSettings: update xdroidUI
    - Redesigned About Phone section
    - Import xdroidUI assets
    - Rearrange some preferences

--- src/com/android/settings/deviceinfo/hardwareinfo/HardwareInfoFragment.java
+++ src/com/android/settings/deviceinfo/hardwareinfo/HardwareInfoFragment.java
@@ -25,6 +25,13 @@ import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settingslib.search.SearchIndexable;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+
+import com.android.settings.deviceinfo.imei.ImeiInfoPreferenceController;
+import com.android.settings.deviceinfo.simstatus.SimStatusPreferenceController;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,6 +50,24 @@ public class HardwareInfoFragment extends DashboardFragment {
     @Override
     protected int getPreferenceScreenResId() {
         return R.xml.hardware_info;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        use(ImeiInfoPreferenceController.class).setHost(this /* parent */);
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, this /* fragment */, getSettingsLifecycle());
+    }
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(
+            Context context, HardwareInfoFragment fragment, Lifecycle lifecycle) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new SimStatusPreferenceController(context, fragment));
+        return controllers;
     }
 
     @Override
