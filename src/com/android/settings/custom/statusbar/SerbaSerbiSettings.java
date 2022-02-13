4b8cb1fec842bd65d1d0267829793edc5296bbf7
Joey Huab <joey@evolution-x.org>
2022-02-13 14:52:44 +0900

    Settings: User toggle for StreamProp [2/2]
    Signed-off-by: ghou8s <ghou8s@gmail.com>

--- src/com/android/settings/custom/statusbar/SerbaSerbiSettings.java
+++ src/com/android/settings/custom/statusbar/SerbaSerbiSettings.java
@@ -44,11 +44,14 @@ public class SerbaSerbiSettings extends SettingsPreferenceFragment
 
     private static final String KEY_GAMES_SPOOF = "use_games_spoof";
     private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
+    private static final String KEY_STREAM_SPOOF = "use_stream_spoof";
     private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
     private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
+    private static final String SYS_STREAM_SPOOF = "persist.sys.pixelprops.streaming";
 
     private SwitchPreference mGamesSpoof;
     private SwitchPreference mPhotosSpoof;
+    private SwitchPreference mStreamSpoof;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -66,6 +69,10 @@ public class SerbaSerbiSettings extends SettingsPreferenceFragment
         mPhotosSpoof = (SwitchPreference) prefScreen.findPreference(KEY_PHOTOS_SPOOF);
         mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
         mPhotosSpoof.setOnPreferenceChangeListener(this);
+
+        mStreamSpoof = (SwitchPreference) findPreference(KEY_STREAM_SPOOF);
+        mStreamSpoof.setChecked(SystemProperties.getBoolean(SYS_STREAM_SPOOF, true));
+        mStreamSpoof.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -85,6 +92,10 @@ public class SerbaSerbiSettings extends SettingsPreferenceFragment
             boolean value = (Boolean) newValue;
             SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "true" : "false");
             return true;
+        } else if (preference == mStreamSpoof) {
+            boolean value = (Boolean) newValue;
+            SystemProperties.set(SYS_STREAM_SPOOF, value ? "true" : "false");
+            return true;
         }
         return true;
     }
@@ -92,6 +103,7 @@ public class SerbaSerbiSettings extends SettingsPreferenceFragment
     public static void reset(Context mContext) {
         SystemProperties.set(SYS_GAMES_SPOOF, "false");
         SystemProperties.set(SYS_PHOTOS_SPOOF, "true");
+        SystemProperties.set(SYS_STREAM_SPOOF, "true");
     }
 
     @Override
