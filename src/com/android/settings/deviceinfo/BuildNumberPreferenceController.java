d01b29d7c563198aab2aafd664ad9c7e59028b23
ghou8s <ghou8s@gmail.com>
2022-04-04 19:01:08 +0000

    Revert "Settings: Append PixelExperience version on build display id"
    This reverts commit 51640d9002461c3b89b6d9b82dd00bcf95f6fec9.

--- src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
+++ src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
@@ -76,14 +76,7 @@ public class BuildNumberPreferenceController extends BasePreferenceController im
 
     @Override
     public CharSequence getSummary() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
-        String AOSQPVersion = VersionUtils.getAOSQPVersion();
-        if (!AOSQPVersion.equals("")){
-            sb.append("\n");
-            sb.append(AOSQPVersion);
-        }
-        return sb.toString();
+        return BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY);
     }
 
     @Override
