DirtyUnicorns/android_packages_apps_Settings

Expose DevSettings RunningServices so we can run it from ActivityPicker
to quickly check running background apps and services

it'll be com.android.settings.Settings$DevRunningServicesActivity
in Navbar/ActiveEdge App picker list when long pressing on "Settings" app

Change-Id: I10277b18beed5ae2e3c02f5e16fc9928bb3101f9

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -40,6 +40,7 @@ import com.android.settings.applications.DirectoryAccessDetails;
 import com.android.settings.applications.ManageDomainUrls;
 import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.applications.RunningServices;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.VrListenerSettings;
 import com.android.settings.applications.appinfo.AppInfoDashboardFragment;
@@ -265,7 +266,8 @@ public class SettingsGateway {
             DirectoryAccessDetails.class.getName(),
             ToggleBackupSettingFragment.class.getName(),
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
-            DirtyTweaks.class.getName()
+            DirtyTweaks.class.getName(),
+            RunningServices.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -305,5 +307,6 @@ public class SettingsGateway {
             Settings.EnterprisePrivacySettingsActivity.class.getName(),
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.DirtyTweaksActivity.class.getName(),
+            Settings.DevRunningServicesActivity.class.getName(),
     };
 }

keepQASSA/packages_apps_Settings

Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -36,6 +36,7 @@ import com.android.settings.accounts.ManagedProfileSettings;
 import com.android.settings.applications.AppAndNotificationDashboardFragment;
 import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.applications.RunningServices;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.appinfo.AppInfoDashboardFragment;
 import com.android.settings.applications.appinfo.DrawOverlayDetails;
@@ -294,7 +295,8 @@ public class SettingsGateway {
             DarkModeSettingsFragment.class.getName(),
             LiveDisplaySettings.class.getName(),
             keepQASSA.class.getName(),
-            GamingMode.class.getName()
+            GamingMode.class.getName(),
+            RunningServices.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -335,5 +337,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.keepQASSAActivity.class.getName(),
             Settings.GamingModeActivity.class.getName(),
+            Settings.DevRunningServicesActivity.class.getName(),
     };
 }