sharanpai3/platform_packages_apps_Settings

Settings: UI Clean-up [2/2] * Adapt to Octavi
Signed-off-by: Rahul <rahul731999@gmail.com>

src/com/android/settings/accessibility/ToggleFeaturePreferenceFragment.java
@@ -196,7 +196,7 @@ public abstract class ToggleFeaturePreferenceFragment extends SettingsPreference
         if (mSettingsTitle != null && mSettingsIntent != null) {
             mSettingsPreference = new Preference(getPrefContext());
             mSettingsPreference.setTitle(mSettingsTitle);
-            mSettingsPreference.setIconSpaceReserved(true);
+            mSettingsPreference.setIconSpaceReserved(false);
             mSettingsPreference.setIntent(mSettingsIntent);
         }
 

keepQASSA/packages_apps_Settings

src/com/android/settings/accessibility/ToggleFeaturePreferenceFragment.java
@@ -67,7 +67,7 @@ public abstract class ToggleFeaturePreferenceFragment extends SettingsPreference
             PreferenceScreen preferenceScreen = getPreferenceScreen();
             Preference settingsPref = new Preference(preferenceScreen.getContext());
             settingsPref.setTitle(mSettingsTitle);
-            settingsPref.setIconSpaceReserved(true);
+            settingsPref.setIconSpaceReserved(false);
             settingsPref.setIntent(mSettingsIntent);
             preferenceScreen.addPreference(settingsPref);
         }