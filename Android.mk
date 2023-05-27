d9b65bdd66fe917ba2f228d2ddd0cb0fc509f6dc
ghou8s <ghou8s@gmail.com>
2023-05-27 17:36:03 +0000

    Revert "return to default setting interface [1/2]"
    This reverts commit 1dc3643b3c025f927a2aa5969e3f7ae6f08d9a21.

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-        $(call all-logtags-files-under, src)
+        $(call all-logtags-files-under, src, src-xdui)
 
 LOCAL_MODULE := settings-logtags
 
@@ -20,7 +20,7 @@ LOCAL_REQUIRED_MODULES := privapp_whitelist_com.android.settings
 LOCAL_MODULE_TAGS := optional
 LOCAL_USE_AAPT2 := true
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES := $(call all-java-files-under, src, src-xdui)
 LOCAL_SRC_FILES += $(call all-java-files-under, ../keepQASSA/src)
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
@@ -56,6 +56,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     zxing-core-1.7
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
+    $(LOCAL_PATH)/res-xdui \
     packages/apps/TouchGestures/res \
     packages/apps/keepQASSA/res
 
